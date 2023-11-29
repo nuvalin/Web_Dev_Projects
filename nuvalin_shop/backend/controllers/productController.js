6b1ecc5d9ad84c74acd6c0ad2da259cfb135284b
nuvalin <nuvzmoodley@gmail.com>
2023-11-30 00:28:44 +0200

    new comit

--- nuvalin_shop/backend/controllers/productController.js
+++ nuvalin_shop/backend/controllers/productController.js
@@ -12,11 +12,60 @@ exports.newProduct = async (req, res, next) => {
 }
 
 
+//Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
 
-exports.getProducts = (req, res,next) => {
+    const products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: 'This route will show all products in database'
+        count: products.length,
+        products
+    })
+
+}
+
+// Get single product details   =>   /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
+
+// Update Product   =>   /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found',
+        })
+
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
     })
 
 }
--- nuvalin_shop/backend/routes/product.js
+++ nuvalin_shop/backend/routes/product.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router();
 
 
-const { getProducts, newProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
 
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
+
 
 router.route('/product/new').post(newProduct);
 
