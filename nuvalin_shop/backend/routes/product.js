a3fc6c545de0e4b24e7d919e00263f7c45ee367d
nuvalin <nuvzmoodley@gmail.com>
2023-11-30 01:50:23 +0200

    delete comit

--- nuvalin_shop/backend/routes/product.js
+++ nuvalin_shop/backend/routes/product.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router();
 
 
-const { getProducts, newProduct, getSingleProduct,updateProduct } = require('../controllers/productController')
+const { 
+    getProducts,
+     newProduct,
+      getSingleProduct,
+      updateProduct,
+      deleteProduct 
+    } = require('../controllers/productController')
 
 
 router.route('/products').get(getProducts);
@@ -11,7 +17,9 @@ router.route('/product/:id').get(getSingleProduct);
 
 router.route('/admin/product/new').post(newProduct);
 
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id')
+               .put(updateProduct)
+               .delete(deleteProduct);
 
 
 module.exports = router;
